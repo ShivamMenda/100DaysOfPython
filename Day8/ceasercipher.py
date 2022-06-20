b845ce126fb5284333d196a04c2754009a760639
Shivam Menda <shivammenda2002@gmail.com>
2022-06-21 00:10:47 +0530

    Day 8 Complete!

---
+++ Day8/ceasercipher.py
@@ -0,0 +1,107 @@
+
+from sqlalchemy import false
+
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+                                        
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+print(logo)
+
+# #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+# def encrypt(plain_text,shift_amount):
+#     cipher_text=""
+#     for letter in plain_text:
+#         position=alphabet.index(letter)
+#         new_position=position+shift_amount
+#         new_letter=alphabet[new_position]
+#         cipher_text+=new_letter
+#     print(f"The encoded text is {cipher_text}")
+
+#     #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
+#     #e.g. 
+#     #plain_text = "hello"
+#     #shift = 5
+#     #cipher_text = "mjqqt"
+#     #print output: "The encoded text is mjqqt"
+#  ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+
+# #TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
+
+# #TODO-4: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
+
+#   #TODO-5: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.  
+#   #e.g. 
+#   #cipher_text = "mjqqt"
+#   #shift = 5
+#   #plain_text = "hello"
+#   #print output: "The decoded text is hello"
+
+# def decrypt(text,shift):
+#     decrypt_text=""
+#     for l in text:
+#         pos=alphabet.index(l)
+#         new_pos=pos-shift
+#         new_letter=alphabet[new_pos]
+#         decrypt_text+=new_letter
+#     print(f"The decoded text is {decrypt_text}")
+
+
+# #TODO-6: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'direction' variable. You should be able to test the code to encrypt *AND* decrypt a message.
+# if(direction=="encode"):
+#     encrypt(text,shift)
+# elif(direction=="decode"):
+#     decrypt(text,shift)
+# else:
+#     print("Invalid message please try again!")
+
+#refactoring
+def ceaser(text,shift,direction):
+    cipher=""
+    plain=""
+    flag=0
+    for l in text:
+        pos=alphabet.index(l)
+        if(direction=="encode"):
+            flag=1
+            new_pos=pos+shift
+            cipher+=alphabet[new_pos]
+        elif(direction=="decode"):
+            flag=2
+            new_pos=pos-shift
+            plain+=alphabet[new_pos]
+        else:
+            flag=0
+    if(flag==1):
+        print(f"The encoded text is {cipher}")
+    elif(flag==2):
+        print(f"The plain text is {plain}")
+    else:
+        print("Invalid input")
+flag=True
+while flag:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift=shift%26        
+    ceaser(text,shift,direction)
+    result=input("Do you want to continue[y/n]?")
+    if(result=="n"):
+        flag=False
+        print("Goodbye!")
+
+
